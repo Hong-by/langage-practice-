26dc0440e1f412afca10379e5766ce75e37de474
HongBY <hby033@naver.com>
2021-05-28 11:16:25 +0900

    05/28 sessiont 관련 강의

---
+++ PHP/basic_syntax/logout.php
@@ -0,0 +1,18 @@
+<?php
+
+  session_start();
+
+  // 세션을 삭제할 때는 unset() 함수를 사용한다
+
+  unset( $_SESSION['userid']);
+
+  // echo "
+  //   <script>
+  //     location.href = 'loop.php'
+  //   </script>
+  // ";
+
+?>
+
+
+<p>넘어온 세션 : <?=$userid?> 입니다.</p>
